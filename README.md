f4d8c411982857368cf2bf3138e5e555cd83e99b
Anoop <anoopndir@gmail.com>
2026-04-15 22:26:56 +0530

    After Deletion

--- README.md
+++
@@ -1,2 +0,0 @@
-# Trial-Folder-1
-Trial Folder 1 objective is to learn and understand the VCS and Github
--- build_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-python -m PyInstaller --windowed --name "Purchase KPI Dashboard" --noconfirm main.py
-
-echo Build complete: dist\Purchase KPI Dashboard\Purchase KPI Dashboard.exe
-pause
